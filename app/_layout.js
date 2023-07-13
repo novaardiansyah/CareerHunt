c5f6e2ebe4b5ed710d7aa4162fbd5c84ac47d3da
novaardiansyah <novaardiansyah78@gmail.com>
2023-07-13 07:25:17 +0700

    Improved the main page and prepared custom fonts for use

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,25 @@
+import { useCallback } from 'react'
 import { Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync()
 
 const Layout = () => {
-  return <Stack />
+  const [fontsLoaded] = useFonts({
+    DMbold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMregular: require('../assets/fonts/DMSans-Regular.ttf'),
+    DMmedium: require('../assets/fonts/DMSans-Medium.ttf')
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) return null
+  return <Stack onLayout={onLayoutRootView} />
 }
 
 export default Layout
